f8c4c0e947076637b349c69e4b83f2b76a085e2c
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-05-19 16:43:16 +0530

    updated

--- components/hero.php
+++ components/hero.php
@@ -30,7 +30,7 @@ $total_slides_count = count($active_slides);
 
     /* 100% Full Width Hero Slider */
     .hero-slider-col {
-        width: 1350px;
+        width: 1500px;
         height: 400px;
         position: relative;
         overflow: hidden;
